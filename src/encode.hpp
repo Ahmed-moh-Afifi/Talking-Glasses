16f6aa6fb3801dfeee4af3c7014f8258698fefde
alchemistsGestalt <zeinhatem@proton.me>
2024-07-24 16:19:35 +0300

    Moved `sendText()' to encode.hpp

--- src/encode.hpp
+++ src/encode.hpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 
-byte *encode(String messsage)
+void sendText(String message)
 {
-    byte *encoded = new byte[messsage.length() * 2];
-    return encoded;
+  byte *encoded = new byte[message.length() + 5];
+  encoded[0] = 0xFD;
+  short length = message.length() + 2;
+  encoded[1] = ((byte *)&length)[1];
+  encoded[2] = ((byte *)&length)[0];
+  encoded[3] = 0x01;
+  encoded[4] = 0x01;
+  memcpy(encoded + 5, message.c_str(), message.length());
+  ttsSerial.write(encoded, message.length() + 5);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "encode.hpp"
 
 void sendText(String message);
 
@@ -31,16 +32,10 @@ void sendText(String message)
   byte *encoded = new byte[message.length() + 5];
   encoded[0] = 0xFD;
   short length = message.length() + 2;
-  // memcpy(encoded + 1, &lenght, 2);
   encoded[1] = ((byte *)&length)[1];
   encoded[2] = ((byte *)&length)[0];
   encoded[3] = 0x01;
   encoded[4] = 0x01;
   memcpy(encoded + 5, message.c_str(), message.length());
   ttsSerial.write(encoded, message.length() + 5);
-
-  // byte thankyou[] = {0xfd, 0x00, 0x0d, 0x01, 0x01, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64};
-  // ttsSerial.write(thankyou, 16);
-
-  // ttsSerial.write(0xfd);
 }
