c7ba1164d3c61cfafa5b8a03b1841845435f0ebb
Ahmed-moh-afifi <ahmed.moh.afifi@gmail.com>
2024-07-24 21:38:46 +0300

    Fixed the unknown error.

--- src/encode.hpp
+++ src/encode.hpp
@@ -5,10 +5,14 @@
 #include <SoftwareSerial.h>
 #include "enum.hpp"
 
-#define CHECK_RESPONSE {  byte response; ttsSerial.read(&response, 1); return response == 0x41;}
+#define CHECK_RESPONSE                 \
+  {                                    \
+    byte response;                     \
+    ttsSerial.readBytes(&response, 1); \
+    return response == 0x41;           \
+  }
 
 SoftwareSerial ttsSerial(10, 11);
-
 
 bool sendText(String message)
 {
@@ -24,57 +28,66 @@ bool sendText(String message)
   CHECK_RESPONSE;
 }
 
-bool set_volume(vol v) {
-  byte *query = {0x5B, 0x76, 0x30 + v, 0x5D};
+bool set_volume(volume v)
+{
+  byte query[] = {0x5B, 0x76, 0x30 + v, 0x5D};
   ttsSerial.write(query, 4);
   CHECK_RESPONSE;
 }
 
-bool set_speed(speed s) {
-  byte *query = {0x5B, 0x76, 0x30 + s, 0x5D};
+bool set_speed(speed s)
+{
+  byte query[] = {0x5B, 0x76, 0x30 + s, 0x5D};
   ttsSerial.write(query, 4);
   CHECK_RESPONSE;
 }
 
-bool set_tone(tone t) {
-  byte *query = {0x5B, 0x76, 0x30 + t, 0x5D};
+bool set_tone(sound_tone t)
+{
+  byte query[] = {0x5B, 0x76, 0x30 + t, 0x5D};
   ttsSerial.write(query, 4);
   CHECK_RESPONSE;
 }
 
-byte check_status() {
-  byte *query = {0xFD, 0x00, 0x01, 0x21};
+byte check_status()
+{
+  byte query[] = {0xFD, 0x00, 0x01, 0x21};
   ttsSerial.write(query, 4);
   CHECK_RESPONSE;
 }
 
-module_state is_idle() {
+module_state is_idle()
+{
   byte response = check_status();
-  switch (response) {
-    case 0x4E:
-      return m_busy;
-    case 0x4F:
-      return m_idle;
-    default:
-      return m_error;
+  switch (response)
+  {
+  case 0x4E:
+    return m_busy;
+  case 0x4F:
+    return m_idle;
+  default:
+    return m_error;
   }
 }
 
-bool send_ring(ring r) {
-  byte *query = {0x72, 0x69, 0x6E, 0x67, 0x5F, 0x31 + r};
+bool send_ring(ring r)
+{
+  byte query[] = {0x72, 0x69, 0x6E, 0x67, 0x5F, 0x31 + r};
   ttsSerial.write(query, 6);
   CHECK_RESPONSE;
 }
 
-bool send_msg_tone(msg m) {
-  byte *query = {0x6D, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x5F, 0x31 + m};
+bool send_msg_tone(msg m)
+{
+  byte query[] = {0x6D, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x5F, 0x31 + m};
   ttsSerial.write(query, 9);
   CHECK_RESPONSE;
 }
 
-bool send_alert(alert a) {
-  byte *query = {0x61, 0x6C, 0x65, 0x72, 0x74, 0x5F, 0x31 + a};
-  ttsSerial.wirte(query, 7);
+bool send_alert(alert a)
+{
+  byte query[] = {0x61, 0x6C, 0x65, 0x72, 0x74, 0x5F, 0x31 + a};
+  ttsSerial.write(query, 7);
   CHECK_RESPONSE;
 }
 
